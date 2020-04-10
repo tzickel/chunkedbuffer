8a8230d3504c63a706f130c1d982ecf6d4813f2c
tz-rnd <63101707+tz-rnd@users.noreply.github.com>
2020-04-10 19:39:46 +0300

    almost

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("chunkedbuffer/*.pyx", compiler_directives={'language_level' : "3"}))
+setup(ext_modules=cythonize("chunkedbuffer/*.pyx", compiler_directives={"language_level" : "3"}))
+# TODO (cythonize) validate all code for this
+#setup(ext_modules=cythonize("chunkedbuffer/*.pyx", compiler_directives={"language_level" : "3", "boundscheck": False, "wraparound": False, "initializedcheck": False}))

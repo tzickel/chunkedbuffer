d283cbae5c157c6af6b2f66ab34b8bb177c961b1
tzickel <tzickel@users.noreply.github.com>
2020-04-17 21:48:50 +0300

    fixing setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import os
 
 coverage = os.environ.get('CYTHON_COVERAGE', False) in ('1', 'true', '"true"')
 if coverage:
-    setup(ext_modules=cythonize("chunkedbuffer/*.pyx", force=True, compiler_directives={'linetrace': True}))
+    setup(ext_modules=cythonize("chunkedbuffer/*.pyx", language_level=3, force=True, compiler_directives={'linetrace': True}))
 else:
-    setup(ext_modules=cythonize("chunkedbuffer/*.pyx", force=True))
+    setup(ext_modules=cythonize("chunkedbuffer/*.pyx", language_level=3, force=True, compiler_directives={'boundscheck': False, 'wraparound': False, 'initializedcheck': False, 'always_allow_keywords': False}))
